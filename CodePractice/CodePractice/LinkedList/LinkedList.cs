9f52750d1357ead75b9f3f6198accfcbe2546d5e
Abhishek Bhol <abhol@microsoft.com>
2021-08-13 19:06:28 +0530

    linkedlist new functionalities

--- CodePractice/CodePractice/LinkedList/LinkedList.cs
+++ CodePractice/CodePractice/LinkedList/LinkedList.cs
@@ -117,6 +117,197 @@ namespace CodePractice.LinkedList
 
         #endregion
 
+        #region Nth Node
+
+        public Node NthNodeFromStart(int n)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            int count = 1;
+            Node tmp = head;
+
+            while(count < n && tmp != null)
+            {
+                tmp = tmp.next;
+                count++;
+            }
+
+            if(count == n)
+            {
+                return tmp;
+            }
+
+            return null;
+        }
+
+        public Node NthNodeFromEnd(int n)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            int count = 1;
+            Node tmp = head;
+
+            while(tmp != null && count < n)
+            {
+                tmp = tmp.next;
+                count++;
+            }
+
+            if(tmp == null)
+            {
+                return null;
+            }
+
+            Node tmp2 = head;
+            while(tmp.next != null)
+            {
+                tmp = tmp.next;
+                tmp2 = tmp2.next;
+            }
+
+            return tmp2;
+        }
+
+        #endregion
+
+        #region Middle Element
+
+        public Node MiddleElement()
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            if(head.next == null)
+            {
+                return head;
+            }
+
+            Node slowPtr = head;
+            Node fastPtr = head;
+
+            while(fastPtr != null && fastPtr.next != null)
+            {
+                fastPtr = fastPtr.next.next;
+                slowPtr = slowPtr.next;
+            }
+
+            return slowPtr;
+        }
+
+        #endregion
+
+        #region Loop
+
+        public bool HasLoop()
+        {
+            Node slowPtr = head;
+            Node fastPtr = head;
+
+            while (fastPtr != null && fastPtr.next != null)
+            {
+                fastPtr = fastPtr.next.next;
+                slowPtr = slowPtr.next;
+
+                if (fastPtr == slowPtr)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region Palindrome
+
+        public bool IsPalindrome()
+        {
+            if(head == null)
+            {
+                return false;
+            }
+
+            if(head.next == null)
+            {
+                return true;
+            }
+
+            var stack = new Stack<int>();
+            Node slowPtr = head;
+            Node fastPtr = head;
+            stack.Push(slowPtr.data);
+
+            while (fastPtr != null && fastPtr.next != null)
+            {
+                fastPtr = fastPtr.next.next;
+                slowPtr = slowPtr.next;
+                stack.Push(slowPtr.data);
+            }
+
+            if(fastPtr == null)
+            {
+                stack.Pop();
+            }
+
+            while (stack.Count > 0)
+            {
+                var val = stack.Pop();
+                if(val != slowPtr.data)
+                {
+                    return false;
+                }
+                slowPtr = slowPtr.next;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Duplicate
+
+        public void RemoveAllDuplicates()
+        {
+            var set = new HashSet<int>();
+
+            if(head == null || head.next == null)
+            {
+                return;
+            }
+
+            var tmp = head.next;
+            var prev = head;
+            set.Add(prev.data);
+
+            while(tmp != null && prev != null)
+            {
+                if(set.Contains(tmp.data))
+                {
+                    prev.next = tmp.next;
+                    size--;
+                }
+                else
+                {
+                    prev = prev.next;
+                    set.Add(prev.data);
+                }
+                tmp = tmp.next;
+            }
+        }
+
+        #endregion
+
+        // intersection, reverse
+
     }
 
 }
--- CodePractice/TestingCode/LinkedListTests.cs
+++ CodePractice/TestingCode/LinkedListTests.cs
@@ -25,7 +25,6 @@ namespace TestingCode
             Assert.AreEqual(ll.head.next.next.data, 3);
             Assert.AreEqual(ll.head.next.next.next.data, 4);
             Assert.AreEqual(ll.head.next.next.next.next.data, 5);
-
         }
 
         [Test]
@@ -56,5 +55,184 @@ namespace TestingCode
             Assert.AreEqual(ll.size, 0);
             Assert.Null(ll.head);
         }
+
+        [Test]
+        public void TestNthNodeFromStartAllCases()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtLast(4);
+            ll.InsertAtFirst(1);
+
+            var node = ll.NthNodeFromStart(2);
+            Assert.AreEqual(node.data, 2);
+
+            node = ll.NthNodeFromStart(1);
+            Assert.AreEqual(node.data, 1);
+
+            node = ll.NthNodeFromStart(4);
+            Assert.AreEqual(node.data, 4);
+
+            node = ll.NthNodeFromStart(5);
+            Assert.Null(node);
+        }
+
+        [Test]
+        public void TestNthNodeFromStart1Node()
+        {
+            var ll = new LinkedList(3);
+
+            var node = ll.NthNodeFromStart(2);
+            Assert.Null(node);
+
+            node = ll.NthNodeFromStart(1);
+            Assert.AreEqual(node.data, 3);
+        }
+
+        [Test]
+        public void TestNthNodeFromEndAllCases()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtLast(4);
+            ll.InsertAtFirst(1);
+
+            var node = ll.NthNodeFromEnd(2);
+            Assert.AreEqual(node.data, 3);
+
+            node = ll.NthNodeFromEnd(3);
+            Assert.AreEqual(node.data, 2);
+
+            node = ll.NthNodeFromEnd(4);
+            Assert.AreEqual(node.data, 1);
+
+            node = ll.NthNodeFromEnd(1);
+            Assert.AreEqual(node.data, 4);
+
+            node = ll.NthNodeFromEnd(5);
+            Assert.Null(node);
+        }
+
+        [Test]
+        public void TestNthNodeFromEnd1Node()
+        {
+            var ll = new LinkedList(3);
+
+            var node = ll.NthNodeFromEnd(2);
+            Assert.Null(node);
+
+            node = ll.NthNodeFromEnd(1);
+            Assert.AreEqual(node.data, 3);
+        }
+
+        [Test]
+        public void TestMiddleElement()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtFirst(1);
+
+            var node = ll.MiddleElement();
+            Assert.AreEqual(node.data, 2);
+
+            ll.InsertAtLast(4);
+            node = ll.MiddleElement();
+            Assert.AreEqual(node.data, 3);
+        }
+
+        [Test]
+        public void TestMiddleElement1element()
+        {
+            var ll = new LinkedList(3);
+
+            var node = ll.MiddleElement();
+            Assert.AreEqual(node.data, 3);
+
+            ll.DeleteFromEnd();
+            node = ll.MiddleElement();
+            Assert.Null(node);
+        }
+
+        [Test]
+        public void TestPalindrome()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtLast(2);
+
+            var val = ll.IsPalindrome();  // 2->3->2
+            Assert.True(val);
+
+            ll.InsertAfter(ll.head.next, 3); // 2->3->3->2
+            val = ll.IsPalindrome();
+            Assert.True(val);
+
+            ll.DeleteFromStart(); // 3->3->2
+            val = ll.IsPalindrome();
+            Assert.False(val);
+
+            ll.DeleteFromEnd(); // 3->3
+            val = ll.IsPalindrome();
+            Assert.True(val);
+
+            ll.DeleteFromEnd(); // 3
+            val = ll.IsPalindrome();
+            Assert.True(val);
+        }
+
+        [Test]
+        public void TestDuplicates1()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtFirst(2);
+            ll.InsertAtLast(2);
+            ll.InsertAtLast(3);
+            ll.InsertAtLast(2);
+
+            ll.RemoveAllDuplicates();
+            var count = ll.size;
+            Assert.AreEqual(2, count);
+            Assert.AreEqual(2, ll.head.data);
+            Assert.AreEqual(3, ll.head.next.data);
+        }
+
+        [Test]
+        public void TestDuplicates2()
+        {
+            var ll = new LinkedList(2);
+            ll.InsertAtFirst(2);
+            ll.InsertAtFirst(2);
+            ll.InsertAtFirst(2);
+
+            ll.RemoveAllDuplicates();
+            var count = ll.size;
+            Assert.AreEqual(1, count);
+            Assert.AreEqual(2, ll.head.data);
+        }
+
+        [Test]
+        public void TestDuplicates3()
+        {
+            var ll = new LinkedList(3);
+            ll.InsertAtFirst(2);
+            ll.InsertAtFirst(2);
+            ll.InsertAtLast(4);
+            ll.InsertAtLast(4);
+            ll.InsertAtLast(4);
+            ll.InsertAtLast(5);
+            ll.InsertAtLast(4);
+            ll.InsertAtLast(3);
+            ll.InsertAtLast(5);
+
+            ll.RemoveAllDuplicates();
+            var count = ll.size;
+            Assert.AreEqual(4, count);
+            Assert.AreEqual(2, ll.head.data);
+            Assert.AreEqual(3, ll.head.next.data);
+            Assert.AreEqual(4, ll.head.next.next.data);
+            Assert.AreEqual(5, ll.head.next.next.next.data);
+        }
+
     }
 }
